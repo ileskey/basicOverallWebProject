8a77bea41de5ecf700294a6bfce865616517c52b
ileskey <rosette350706@naver.com>
2026-01-14 13:54:14 +0900

    chore: Deleted unneeded code

--- basic-frontend/src/App.tsx
+++ basic-frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
@@ -9,18 +8,6 @@ import Practice from "./pages/practice";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-    const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
-        console.log("버튼 클릭!");
-    };
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value);
-    };
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log("폼 제출!");
-    };
-
     return (
         <>
             <Header />
@@ -52,30 +39,7 @@ function App() {
                     </Routes>
                 </div>
             </div>
-            <div className="card">
-                <p>count is {count}</p>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count up
-                </button>
-                <button onClick={() => setCount(0)}>clear</button>
-                <button onClick={() => setCount((count) => count - 1)}>
-                    count down
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <div className="card">
-                <button onClick={handleClick}>클릭</button>
-                <input onChange={handleChange}></input>
-                <form onSubmit={handleSubmit}>
-                    <button type="submit">제출</button>
-                </form>
-            </div>
 
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
             <Link to="/practice">연습용</Link>
             <Routes>
                 <Route path="/practice" element={<Practice />} />
