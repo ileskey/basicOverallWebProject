1d9b10e321b2bd284670fe50e237296b46716f74
ileskey <rosette350706@naver.com>
2026-01-20 17:50:43 +0900

    refactor: Data loading style changed to use loader

--- basic-frontend/src/pages/BookListPage.tsx
+++ basic-frontend/src/pages/BookListPage.tsx
@@ -1,20 +1,10 @@
-import { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 import { fetchBooks, fetchBookById, fetchBooksByCategory } from "../data/books";
 import BookCard from "../components/BookCard";
 import type { Book } from "../types";
 
 function BookListPage() {
-    const [books, setBooks] = useState<Book[]>();
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        fetchBooks()
-            .then((data) => setBooks(data))
-            .catch((err) => console.error(err))
-            .finally(() => setLoading(false));
-    }, []);
-
-    if (loading) return <p>로딩 중...</p>;
+    const books = useLoaderData() as Book[];
 
     if (books)
         return (
