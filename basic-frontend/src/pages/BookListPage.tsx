978576ccb015e8bb229b8e8c8b2e1d92bccc1287
ileskey <rosette350706@naver.com>
2026-01-22 17:33:36 +0900

    design: Booklist page designed

--- basic-frontend/src/pages/BookListPage.tsx
+++ basic-frontend/src/pages/BookListPage.tsx
@@ -1,22 +1,50 @@
-import { useLoaderData } from "react-router-dom";
-import { fetchBooks, fetchBookById, fetchBooksByCategory } from "../data/books";
+import { useLoaderData, useParams, Link } from "react-router-dom";
 import BookCard from "../components/BookCard";
 import type { Book } from "../types";
-
+import styles from "./BookListPage.module.css";
 function BookListPage() {
     const books = useLoaderData() as Book[];
-
-    if (books)
-        return (
-            <div>
-                <h1>제품 목록</h1>
-                <div>
-                    {books.map((book) => (
-                        <BookCard key={book.id} book={book} />
-                    ))}
-                </div>
+    const { category } = useParams();
+    const getButtonClass = (isActive: boolean) => {
+        return isActive
+            ? `${styles.filterButton} ${styles.filterButtonActive}`
+            : styles.filterButton;
+    };
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                {category ? `${category} 카테고리` : "전체 책 목록"}
+            </h1>
+            <div className={styles.filterButton}>
+                <Link to="/books" className={getButtonClass(!category)}>
+                    전체
+                </Link>
+                <Link
+                    to="/books/category/programming"
+                    className={getButtonClass(category === "programming")}
+                >
+                    프로그래밍
+                </Link>
+                <Link
+                    to="/books/category/novel"
+                    className={getButtonClass(category === "novel")}
+                >
+                    소설
+                </Link>
+                <Link
+                    to="/books/category/science"
+                    className={getButtonClass(category === "science")}
+                >
+                    과학
+                </Link>
             </div>
-        );
+            <div className={styles.booksGrid}>
+                {books.map((book) => (
+                    <BookCard key={book.id} book={book} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default BookListPage;
